dc04fd02d2b5277d6b1faab78791acc34b6805c0
Abhilash <mgabhilash@outlook.com>
2021-05-20 14:35:22 +0530

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     public class AppUser
@@ -6,5 +9,22 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+         public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        // Automapper will automatically map any field named Age if it finds a method name GetAge, this is commented as it interacts with the mapping query in user repository and brings up all fields
+        // public int GetAge() 
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
